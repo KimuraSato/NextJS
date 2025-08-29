5610a5c718e990272ea8c82bd33448bade0ce787
Kimura <cauasorattomendes@gmail.com>
2025-08-29 20:15:58 -0300

    first commit

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,27 @@
+import {lusitana} from "@/app/ui/fonts"
+import RevenueChart from "../../ui/dashboard/revenue-chart"
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+export default async function Page(){ 
+    const latestInvoices = await fetchLatestInvoices();
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl 
+            md:text-2x1`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+
+            </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+        </main>
+    )
+}
